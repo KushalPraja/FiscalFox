c88358ee8c40d6b8fe907d3c42ebec572b676072
kushal <kushpraja6@gmail.com>
2025-01-29 13:25:34 -0500

    Refactor authentication logic; switch to postgres for user retrieval and improve error handling

--- auth.ts
+++ auth.ts
@@ -2,14 +2,16 @@ import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
-import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
+import postgres from 'postgres';
+ 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
@@ -29,15 +31,14 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          
           const passwordsMatch = await bcrypt.compare(password, user.password);
-
+ 
           if (passwordsMatch) return user;
         }
-        
-        console.error('Failed to authorize user:', parsedCredentials.error);
+        console.log('Invalid credentials');
         return null;
       },
     }),
-    
   ],
 });
